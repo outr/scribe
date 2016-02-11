cc4b48e90ce770749d72cac2f7c2ea6cf1f85147
Matt Hicks <matt@outr.com>
2016-02-11 11:07:32 -0600

    slf4j: Finished preliminary integration and basic tests.

--- jvm/src/main/scala/com/outr/scribe/Platform.scala
+++ jvm/src/main/scala/com/outr/scribe/Platform.scala
@@ -1,8 +1,60 @@
 package com.outr.scribe
 
+import scala.annotation.tailrec
+
 object Platform {
+  private val NativeMethod = -2
+
   val LineSeparator = System.getProperty("line.separator")
 
   def formatDate(pattern: String, timestamp: Long): String =
     pattern.format(timestamp)
+
+  /**
+    * Converts a Throwable to a String representation for output in logging.
+    */
+  @tailrec
+  final def throwable2String(t: Throwable,
+                             primaryCause: Boolean = true,
+                             b: StringBuilder = new StringBuilder): String = {
+    if (!primaryCause) {
+      b.append("Caused by: ")
+    }
+    b.append(t.getClass.getName)
+    if (Option(t.getLocalizedMessage).nonEmpty) {
+      b.append(": ")
+      b.append(t.getLocalizedMessage)
+    }
+    b.append(System.getProperty("line.separator"))
+    writeStackTrace(b, t.getStackTrace)
+    if (Option(t.getCause).isEmpty) {
+      b.toString()
+    } else {
+      throwable2String(t.getCause, primaryCause = false, b = b)
+    }
+  }
+
+  @tailrec
+  private def writeStackTrace(b: StringBuilder, elements: Array[StackTraceElement]): Unit = {
+    if (elements.nonEmpty) {
+      val head = elements.head
+      b.append("\tat ")
+      b.append(head.getClassName)
+      b.append('.')
+      b.append(head.getMethodName)
+      b.append('(')
+      if (head.getLineNumber == NativeMethod) {
+        b.append("Native Method")
+      } else {
+        b.append(head.getFileName)
+        if (head.getLineNumber > 0) {
+          b.append(':')
+          b.append(head.getLineNumber)
+        }
+      }
+      b.append(')')
+      b.append(System.getProperty("line.separator"))
+      writeStackTrace(b, elements.tail)
+    }
+  }
 }
--- slf4j/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
+++ slf4j/src/main/java/org/slf4j/impl/StaticLoggerBinder.java
@@ -1,18 +1,19 @@
 package org.slf4j.impl;
 
+import com.scribe.slf4j.ScribeLoggerFactory;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.spi.LoggerFactoryBinder;
 
 public class StaticLoggerBinder implements LoggerFactoryBinder {
     private static final StaticLoggerBinder SINGLETON = new StaticLoggerBinder();
-    private static final String loggerFactoryClassStr = ???
+    private static final String loggerFactoryClassStr = ScribeLoggerFactory.class.getName();
 
     public static String REQUESTED_API_VERSION = "1.7.15";
 
     private final ILoggerFactory loggerFactory;
 
     private StaticLoggerBinder() {
-        loggerFactory = ???;
+        loggerFactory = new ScribeLoggerFactory();
     }
 
     public static StaticLoggerBinder getSingleton() {
