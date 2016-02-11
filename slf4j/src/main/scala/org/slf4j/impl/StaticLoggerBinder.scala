620821dbd6fe486a3e431358525982f478960f75
Matt Hicks <matt@outr.com>
2016-02-11 13:58:26 -0600

    slf4j: Replaced StaticLoggerBinder.java with StaticLoggerBinder.scala

---
+++ slf4j/src/main/scala/org/slf4j/impl/StaticLoggerBinder.scala
@@ -0,0 +1,20 @@
+package org.slf4j.impl
+
+import com.scribe.slf4j.ScribeLoggerFactory
+import org.slf4j.ILoggerFactory
+import org.slf4j.spi.LoggerFactoryBinder
+
+class StaticLoggerBinder private() extends LoggerFactoryBinder {
+  private val factory = new ScribeLoggerFactory
+  private val classString = classOf[ScribeLoggerFactory].getName
+
+  override def getLoggerFactory: ILoggerFactory = factory
+
+  override def getLoggerFactoryClassStr: String = classString
+}
+
+object StaticLoggerBinder extends StaticLoggerBinder {
+  val REQUESTED_API_VERSION = "1.7.15"
+
+  def getSingleton: StaticLoggerBinder = this
+}
