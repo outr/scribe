ff0316b46fd97d0ea681d50725a46b329fedb6bf
Jeff Shaw <shawjef3@gmail.com>
2017-11-28 21:59:29 -0500

    Use nio Path instead of File for log locations.
    This is a breaking change.

--- jvm/src/test/scala/spec/FileLoggingSpec.scala
+++ jvm/src/test/scala/spec/FileLoggingSpec.scala
@@ -1,36 +1,36 @@
 package spec
 
-import java.io.File
-
+import java.nio.file.{Files, Path, Paths}
 import scribe.formatter.Formatter
 import scribe.writer.FileWriter
 import scribe.{LogHandler, Logger}
 import org.scalatest.{Matchers, WordSpec}
 
 import scala.io.Source
-import scala.util.Try
 
 class FileLoggingSpec extends WordSpec with Matchers {
   lazy val fileLogger: Logger = Logger(parentName = None)
-  lazy val logFile: File = new File("logs/test.log")
+  lazy val logFile: Path = Paths.get("logs/test.log")
   lazy val writer: FileWriter = FileWriter.flat("test")
 
   "File Logging" should {
     "configure logging to a temporary file" in {
-      logFile.delete()
+      if (Files.exists(logFile)) {
+        Files.delete(logFile)
+      }
       fileLogger.addHandler(LogHandler(formatter = Formatter.simple, writer = writer))
     }
     "log to the file" in {
       fileLogger.info("Testing File Logger")
     }
     "verify the file was logged to" in {
-      logFile.exists() should be(true)
-      val source = Source.fromFile(logFile)
+      Files.exists(logFile) should be(true)
+      val source = Source.fromFile(logFile.toFile)
       try {
         source.mkString.trim should equal("Testing File Logger")
       } finally {
         source.close()
-        logFile.delete()
+        Files.delete(logFile)
       }
     }
     "close and release the file handle" in {
