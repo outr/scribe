03f9b81b9071a3af0f04ea8e4ff2dde8378c268b
Matt Hicks <matt@outr.com>
2018-01-19 08:27:00 -0600

    Added slf4j and slack modules to publish script.

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt clean +scribeJVM/publishSigned +scribeJS/publishSigned ++2.11.12 scribeNative/publishSigned sonatypeRelease
+sbt clean +scribeJVM/publishSigned +scribeJS/publishSigned +slf4j/publishSigned +slack/publishSigned ++2.11.12 scribeNative/publishSigned sonatypeRelease
