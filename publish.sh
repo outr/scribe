31af01328f94f74f7371f731f2cbd1c0701ce074
Matt Hicks <matt@outr.com>
2018-02-12 12:24:37 -0600

    Release 2.1.0

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sbt clean +macrosJS/publishSigned +macrosJVM/publishSigned +coreJS/publishSigned +coreJVM/publishSigned +extrasJS/publishSigned +extrasJVM/publishSigned +slf4j/publishSigned +slack/publishSigned ++2.11.12 macrosNative/publishSigned coreNative/publishSigned extrasNative/publishSigned sonatypeRelease
+sbt +clean +macrosJS/publishSigned +macrosJVM/publishSigned +coreJS/publishSigned +coreJVM/publishSigned +extrasJS/publishSigned +extrasJVM/publishSigned +slf4j/publishSigned +slack/publishSigned ++2.11.12 macrosNative/publishSigned coreNative/publishSigned extrasNative/publishSigned sonatypeRelease
