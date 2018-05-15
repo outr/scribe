43e95b7c4c7f1eafa8dbd20b289adc851b04c718
Matt Hicks <matt@outr.com>
2018-05-15 15:39:32 -0500

    Added support for functional logging for higher performance

--- core/shared/src/main/scala/scribe/LoggerSupport.scala
+++ core/shared/src/main/scala/scribe/LoggerSupport.scala
@@ -5,18 +5,18 @@ import scala.language.experimental.macros
 trait LoggerSupport {
   def log[M](record: LogRecord[M]): Unit
 
-  def log[M](level: Level, message: M, throwable: Option[Throwable])
+  def log[M](level: Level, message: => M, throwable: Option[Throwable])
             (implicit loggable: Loggable[M]): Unit = macro Macros.log[M]
   
-  def trace[M : Loggable](message: M): Unit = macro Macros.autoLevel[M]
-  def debug[M : Loggable](message: M): Unit = macro Macros.autoLevel[M]
-  def info[M : Loggable](message: M): Unit = macro Macros.autoLevel[M]
-  def warn[M : Loggable](message: M): Unit = macro Macros.autoLevel[M]
-  def error[M : Loggable](message: M): Unit = macro Macros.autoLevel[M]
+  def trace[M : Loggable](message: => M): Unit = macro Macros.autoLevel[M]
+  def debug[M : Loggable](message: => M): Unit = macro Macros.autoLevel[M]
+  def info[M : Loggable](message: => M): Unit = macro Macros.autoLevel[M]
+  def warn[M : Loggable](message: => M): Unit = macro Macros.autoLevel[M]
+  def error[M : Loggable](message: => M): Unit = macro Macros.autoLevel[M]
 
-  def trace[M : Loggable](message: M, t: Throwable): Unit = macro Macros.autoLevel2[M]
-  def debug[M : Loggable](message: M, t: Throwable): Unit = macro Macros.autoLevel2[M]
-  def info[M : Loggable](message: M, t: Throwable): Unit = macro Macros.autoLevel2[M]
-  def warn[M : Loggable](message: M, t: Throwable): Unit = macro Macros.autoLevel2[M]
-  def error[M : Loggable](message: M, t: Throwable): Unit = macro Macros.autoLevel2[M]
+  def trace[M : Loggable](message: => M, t: Throwable): Unit = macro Macros.autoLevel2[M]
+  def debug[M : Loggable](message: => M, t: Throwable): Unit = macro Macros.autoLevel2[M]
+  def info[M : Loggable](message: => M, t: Throwable): Unit = macro Macros.autoLevel2[M]
+  def warn[M : Loggable](message: => M, t: Throwable): Unit = macro Macros.autoLevel2[M]
+  def error[M : Loggable](message: => M, t: Throwable): Unit = macro Macros.autoLevel2[M]
 }
