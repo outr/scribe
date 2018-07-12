8017d97954d872ea025a88744195cd372e373e07
Matt Hicks <matt@outr.com>
2018-07-12 14:08:39 -0500

    Major cleanup of SLF4J LoggerAdapter to fix non-standard formatting that SLF4J supports

--- slf4j/src/main/scala/scribe/slf4j/ScribeLoggerAdapter.scala
+++ slf4j/src/main/scala/scribe/slf4j/ScribeLoggerAdapter.scala
@@ -1,10 +1,9 @@
 package scribe.slf4j
 
-import org.slf4j.{Logger, Marker}
-import org.slf4j.helpers.MarkerIgnoringBase
+import org.slf4j.Logger
+import org.slf4j.helpers.{FormattingTuple, MarkerIgnoringBase, MessageFormatter}
 import org.slf4j.spi.LocationAwareLogger
-
-import scribe.{Logger => ScribeLogger, Platform, Level}
+import scribe.{Level, Logger => ScribeLogger}
 
 class ScribeLoggerAdapter(name: String) extends MarkerIgnoringBase with Logger {
   private def logger = ScribeLogger(name)
@@ -24,63 +23,97 @@ class ScribeLoggerAdapter(name: String) extends MarkerIgnoringBase with Logger {
     case None => logger.log(level, t, None)
   }
 
-  override def warn(msg: String): Unit = logger.warn(msg)
+  override def isTraceEnabled: Boolean = logger.includes(Level.Trace)
 
-  override def warn(format: String, arg: scala.Any): Unit = logger.warn(format.format(arg))
+  override def isDebugEnabled: Boolean = logger.includes(Level.Debug)
 
-  override def warn(format: String, arguments: AnyRef*): Unit = logger.warn(format.format(arguments: _*))
+  override def isInfoEnabled: Boolean = logger.includes(Level.Info)
 
-  override def warn(format: String, arg1: scala.Any, arg2: scala.Any): Unit = logger.warn(format.format(arg1, arg2))
+  override def isWarnEnabled: Boolean = logger.includes(Level.Warn)
 
-  override def warn(msg: String, t: Throwable): Unit = log(Level.Warn, msg, t)
-
-  override def isErrorEnabled: Boolean = true //logger.accepts(Level.Error.value)
-
-  override def isInfoEnabled: Boolean = true //logger.accepts(Level.Info.value)
-
-  override def isDebugEnabled: Boolean = true //logger.accepts(Level.Debug.value)
-
-  override def isTraceEnabled: Boolean = true //logger.accepts(Level.Trace.value)
-
-  override def error(msg: String): Unit = logger.error(msg)
-
-  override def error(format: String, arg: scala.Any): Unit = logger.error(format.format(arg))
-
-  override def error(format: String, arg1: scala.Any, arg2: scala.Any): Unit = logger.error(format.format(arg1, arg2))
-
-  override def error(format: String, arguments: AnyRef*): Unit = logger.error(format.format(arguments: _*))
-
-  override def error(msg: String, t: Throwable): Unit = log(Level.Error, msg, t)
-
-  override def debug(msg: String): Unit = logger.debug(msg)
-
-  override def debug(format: String, arg: scala.Any): Unit = logger.debug(format.format(arg))
-
-  override def debug(format: String, arg1: scala.Any, arg2: scala.Any): Unit = logger.debug(format.format(arg1, arg2))
-
-  override def debug(format: String, arguments: AnyRef*): Unit = logger.debug(format.format(arguments: _*))
-
-  override def debug(msg: String, t: Throwable): Unit = log(Level.Debug, msg, t)
-
-  override def isWarnEnabled: Boolean = true //logger.accepts(Level.Warn.value)
+  override def isErrorEnabled: Boolean = logger.includes(Level.Error)
 
   override def trace(msg: String): Unit = logger.trace(msg)
 
-  override def trace(format: String, arg: scala.Any): Unit = logger.trace(format.format(arg))
+  override def trace(format: String, arg: scala.Any): Unit = {
+    logTuple(Level.Trace, MessageFormatter.format(format, arg))
+  }
 
-  override def trace(format: String, arg1: scala.Any, arg2: scala.Any): Unit = logger.trace(format.format(arg1, arg2))
+  override def trace(format: String, arg1: scala.Any, arg2: scala.Any): Unit = {
+    logTuple(Level.Trace, MessageFormatter.format(format, arg1, arg2))
+  }
 
-  override def trace(format: String, arguments: AnyRef*): Unit = logger.trace(format.format(arguments: _*))
+  override def trace(format: String, arguments: AnyRef*): Unit = {
+    logTuple(Level.Trace, MessageFormatter.arrayFormat(format, arguments.toArray))
+  }
 
   override def trace(msg: String, t: Throwable): Unit = log(Level.Trace, msg, t)
 
+  override def debug(msg: String): Unit = logger.debug(msg)
+
+  override def debug(format: String, arg: scala.Any): Unit = {
+    logTuple(Level.Debug, MessageFormatter.format(format, arg))
+  }
+
+  override def debug(format: String, arg1: scala.Any, arg2: scala.Any): Unit = {
+    logTuple(Level.Debug, MessageFormatter.format(format, arg1, arg2))
+  }
+
+  override def debug(format: String, arguments: AnyRef*): Unit = {
+    logTuple(Level.Debug, MessageFormatter.arrayFormat(format, arguments.toArray))
+  }
+
+  override def debug(msg: String, t: Throwable): Unit = log(Level.Debug, msg, t)
+
   override def info(msg: String): Unit = logger.info(msg)
 
-  override def info(format: String, arg: scala.Any): Unit = logger.info(format.format(arg))
+  override def info(format: String, arg: scala.Any): Unit = {
+    logTuple(Level.Info, MessageFormatter.format(format, arg))
+  }
 
-  override def info(format: String, arg1: scala.Any, arg2: scala.Any): Unit = logger.info(format.format(arg1, arg2))
+  override def info(format: String, arg1: scala.Any, arg2: scala.Any): Unit = {
+    logTuple(Level.Info, MessageFormatter.format(format, arg1, arg2))
+  }
 
-  override def info(format: String, arguments: AnyRef*): Unit = logger.info(format.format(arguments: _*))
+  override def info(format: String, arguments: AnyRef*): Unit = {
+    logTuple(Level.Info, MessageFormatter.arrayFormat(format, arguments.toArray))
+  }
 
   override def info(msg: String, t: Throwable): Unit = log(Level.Info, msg, t)
+
+  override def warn(msg: String): Unit = logger.warn(msg)
+
+  override def warn(format: String, arg: scala.Any): Unit = {
+    logTuple(Level.Warn, MessageFormatter.format(format, arg))
+  }
+
+  override def warn(format: String, arg1: scala.Any, arg2: scala.Any): Unit = {
+    logTuple(Level.Warn, MessageFormatter.format(format, arg1, arg2))
+  }
+
+  override def warn(format: String, arguments: AnyRef*): Unit = {
+    logTuple(Level.Warn, MessageFormatter.arrayFormat(format, arguments.toArray))
+  }
+
+  override def warn(msg: String, t: Throwable): Unit = log(Level.Warn, msg, t)
+
+  override def error(msg: String): Unit = logger.error(msg)
+
+  override def error(format: String, arg: scala.Any): Unit = {
+    logTuple(Level.Error, MessageFormatter.format(format, arg))
+  }
+
+  override def error(format: String, arg1: scala.Any, arg2: scala.Any): Unit = {
+    logTuple(Level.Error, MessageFormatter.format(format, arg1, arg2))
+  }
+
+  override def error(format: String, arguments: AnyRef*): Unit = {
+    logTuple(Level.Error, MessageFormatter.arrayFormat(format, arguments.toArray))
+  }
+
+  override def error(msg: String, t: Throwable): Unit = log(Level.Error, msg, t)
+
+  private def logTuple(level: Level, tuple: FormattingTuple): Unit = {
+    logger.log(level, tuple.getMessage, Option(tuple.getThrowable))
+  }
 }
