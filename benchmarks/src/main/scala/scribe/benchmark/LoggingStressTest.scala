be91311fdbce20526ff0d32297838ef6c17de1d3
Matt Hicks <matt@outr.com>
2018-08-25 12:37:41 -0500

    Continued work on stress test functionality

--- benchmarks/src/main/scala/scribe/benchmark/LoggingStressTest.scala
+++ benchmarks/src/main/scala/scribe/benchmark/LoggingStressTest.scala
@@ -1,12 +1,58 @@
 package scribe.benchmark
 
+import java.nio.file.{Files, Paths}
+import java.util.concurrent.TimeUnit
+
 import scribe.Logger
+import scribe.format.Formatter
+import scribe.writer.FileWriter
+import scribe.writer.file.LogFileMode
 
 object LoggingStressTest {
   def main(args: Array[String]): Unit = {
-    val logger = Logger.empty.orphan()
-    while (true) {
-      logger.info("This is a test")
+    val iterations = 1000000
+    val types = List(
+      "Null" -> nullLogger(),
+      "NIO Simple" -> fileLogger(Formatter.simple, LogFileMode.NIO),
+      "IO Simple" -> fileLogger(Formatter.simple, LogFileMode.IO),
+      "NIO Default" -> fileLogger(Formatter.default, LogFileMode.NIO),
+      "IO Default" -> fileLogger(Formatter.default, LogFileMode.IO)
+    )
+    types.foreach {
+      case (name, logger) => {
+        val elapsed = timed(iterations, logger)
+        scribe.info(s"$iterations for $name in $elapsed seconds")
+      }
+    }
+    scribe.info("Reversing!")
+    types.reverse.foreach {
+      case (name, logger) => {
+        val elapsed = timed(iterations, logger)
+        scribe.info(s"$iterations for $name in $elapsed seconds")
+      }
+    }
+    scribe.info("Completed!")
+  }
+
+  def nullLogger(): Logger = Logger.empty.orphan()
+
+  def fileLogger(formatter: Formatter, mode: LogFileMode): Logger = {
+    val path = Paths.get("logs/file-logging.log")
+    Files.deleteIfExists(path)
+    Logger.empty.orphan().withHandler(formatter, FileWriter().withMode(mode).path(_ => path))
+  }
+
+  def timed(iterations: Int, logger: Logger): Double = {
+    val start = System.nanoTime()
+    stressLogger(iterations, logger)
+    val elapsed = System.nanoTime() - start
+    TimeUnit.MILLISECONDS.convert(elapsed, TimeUnit.NANOSECONDS) / 1000.0
+  }
+
+  def stressLogger(iterations: Int, logger: Logger): Unit = {
+    logger.info("Testing logging")
+    if (iterations > 0) {
+      stressLogger(iterations - 1, logger)
     }
   }
 }
