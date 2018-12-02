f0d2be9b9be80ea124610eaf64d8c6168f685a4e
Matt Hicks <matt@outr.com>
2018-12-02 12:36:17 -0600

    Release 2.7.0

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@
 set -e
 
 sbt +clean +compile
-sbt +testsJS/test +testsJVM/test +slack/test +slf4j/test +slf4j18/test +logstash/test
+sbt +test
 sbt +macrosJS/publishSigned +macrosJVM/publishSigned +coreJS/publishSigned +coreJVM/publishSigned +slf4j/publishSigned +slf4j18/publishSigned +slack/publishSigned +logstash/publishSigned ++2.11.12 macrosNative/publishSigned coreNative/publishSigned
 sbt sonatypeRelease
