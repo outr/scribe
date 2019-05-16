8448bda41da2c31ea5134ed58725d575a6d03a3a
Matt Hicks <matt@outr.com>
2019-05-16 09:10:43 -0500

    Added className to DSL

--- core/shared/src/main/scala/scribe/filter/package.scala
+++ core/shared/src/main/scala/scribe/filter/package.scala
@@ -5,4 +5,5 @@ import scribe.modify.LevelFilter
 package object filter extends FilterBuilder() {
   def level: LevelFilter.type = LevelFilter
   def packageName: PackageNameFilter.type = PackageNameFilter
+  def className: ClassNameFilter.type = ClassNameFilter
 }
