702f763f1bf49847ba35c02678483e09dcb3e2c5
Matt Hicks <matt@courio.com>
2020-10-21 10:53:56 -0700

    Added docs to ConsoleWriter.OnlyPlainText

--- core/shared/src/main/scala/scribe/writer/ConsoleWriter.scala
+++ core/shared/src/main/scala/scribe/writer/ConsoleWriter.scala
@@ -4,6 +4,10 @@ import scribe._
 import scribe.output._
 
 object ConsoleWriter extends Writer {
+  /**
+    * If true, will always write out plain text. If false, will detect platform and output support for rich output. This
+    * presumes that the `Formatter` is not already creating plain text. Defaults to false.
+    */
   var OnlyPlainText: Boolean = false
 
   override def write[M](record: LogRecord[M], output: LogOutput): Unit = synchronized {
