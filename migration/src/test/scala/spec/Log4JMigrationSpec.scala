ae2942a3945e165b1ab6852be63a93bc7e65f8c6
Matt Hicks <matt@courio.com>
2020-11-03 12:21:49 -0800

    Release 3.0.0

--- migration/src/test/scala/spec/Log4JMigrationSpec.scala
+++ migration/src/test/scala/spec/Log4JMigrationSpec.scala
@@ -4,6 +4,8 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import scribe.Log4JMigration
 
+import scala.language.implicitConversions
+
 class Log4JMigrationSpec extends AnyWordSpec with Matchers {
   "Log4JMigration" should {
     "load existing configuration" in {
--- sbt.scalajs1.0.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export SCALAJS_VERSION=1.0.0-M1
-sbt
