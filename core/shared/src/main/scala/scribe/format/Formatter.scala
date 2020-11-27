620da6a966bf82f871829891145afdfe45abdc4b
Matt Hicks <matt@courio.com>
2020-11-27 09:34:58 -0800

    Updated Formatter.enhanced to no longer set the message color to gray to avoid issues on some dark terminal themes (Resolves #142)

--- core/shared/src/main/scala/scribe/format/Formatter.scala
+++ core/shared/src/main/scala/scribe/format/Formatter.scala
@@ -11,7 +11,7 @@ object Formatter {
   lazy val simple: Formatter = formatter"$message$mdc"
   lazy val classic: Formatter = formatter"$date [$threadNameAbbreviated] $level $position - $message$mdc"
   lazy val compact: Formatter = formatter"$date ${string("[")}$levelColored${string("]")} ${green(position)} - $message$mdc"
-  lazy val enhanced: Formatter = formatter"$dateFull ${string("[")}$levelColoredPaddedRight${string("]")} ${green(position)} - ${gray(message)}$mdc"
+  lazy val enhanced: Formatter = formatter"$dateFull ${string("[")}$levelColoredPaddedRight${string("]")} ${green(position)} - $message$mdc"
   lazy val strict: Formatter = formatter"$date [$threadNameAbbreviated] $levelPaddedRight $positionAbbreviated - $message$mdc"
 
   def fromBlocks(blocks: FormatBlock*): Formatter = new FormatBlocksFormatter(blocks.toList)
