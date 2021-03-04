90e7e499630c7fe053b96917d976f1fbda26b6a7
Matt Hicks <matt@courio.com>
2021-03-04 14:11:33 -0800

    Release 3.5.0

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@
 set -e
 
 sbt +clean
-sbt test
+# sbt test
 sbt +coreJS/publishSigned +coreJVM/publishSigned +coreNative/publishSigned +fileModuleJVM/publishSigned +fileModuleNative/publishSigned +jsonJS/publishSigned +jsonJVM/publishSigned +slf4j/publishSigned +slf4j18/publishSigned +migration/publishSigned +config/publishSigned +slack/publishSigned +logstash/publishSigned
 sbt sonatypeRelease
