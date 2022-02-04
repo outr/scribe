29483efde6933dee9614080d88870d1aac69eaea
Kamil Podsiadlo <kpodsiadlo@virtuslab.com>
2022-02-04 12:37:43 +0100

    feat: allow to customize directory for FileWriter logs

--- fileModule/shared/src/main/scala/scribe/file/PathBuilder.scala
+++ fileModule/shared/src/main/scala/scribe/file/PathBuilder.scala
@@ -5,25 +5,34 @@ import scribe.file.path.PathPart
 import java.io.File
 import java.nio.file.Path
 
-case class PathBuilder(parts: List[PathPart]) {
+/**
+  * @param partsList of PathPart's which will be used to build the name of the logfile
+  * @param rootDir directory in which logs will be stored
+  */
+case class PathBuilder(parts: List[PathPart], rootDir: File = PathBuilder.DefaultRootDir) {
+  val rootDirPath = rootDir.getAbsolutePath()
+
   def before(writer: FileWriter): Unit = parts.foreach(_.before(writer))
   def after(writer: FileWriter): Unit = parts.foreach(_.after(writer))
 
   def file(timeStamp: Long): File = {
-    val path = parts.foldLeft(PathBuilder.DefaultPath)((previous, part) => part.current(previous, timeStamp))
+    val path = parts.foldLeft(rootDirPath)((previous, part) => part.current(previous, timeStamp))
     new File(path)
   }
 
   def iterator(): Iterator[File] = parts
-    .foldLeft(Iterator(PathBuilder.DefaultPath))((previous, part) => previous.flatMap(part.all))
+    .foldLeft(Iterator(rootDirPath))((previous, part) => previous.flatMap(part.all))
     .map(new File(_))
 
   def /(part: PathPart): PathBuilder = copy(parts ::: List(part))
 }
 
 object PathBuilder {
-  lazy val DefaultPath: String = new File("logs", "app.log").getAbsolutePath
+  lazy val DefaultRootDir: File = new File("logs", "app.log")
+  lazy val DefaultPath: String = DefaultRootDir.getAbsolutePath
   lazy val Default: PathBuilder = PathBuilder(List(PathPart.SetPath(DefaultPath)))
 
   def static(path: Path): PathBuilder = PathBuilder(List(PathPart.SetPath(path.toString)))
+  def static(fileName: Path, rootDir: Path): PathBuilder = PathBuilder(List(PathPart.SetPath(fileName.toString)), rootDir.toFile())
+  def static(parts: List[PathPart], rootDir: Path): PathBuilder = PathBuilder(parts, rootDir.toFile())
 }
