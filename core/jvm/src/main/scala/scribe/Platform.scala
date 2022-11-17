9f5941a8cb08b376b0994ffef9c8981328b41cf9
Matt Hicks <matt@matthicks.com>
2022-11-17 09:56:23 -0600

    Reworking of Platform on JVM to better handle strange console cases

--- core/jvm/src/main/scala/scribe/Platform.scala
+++ core/jvm/src/main/scala/scribe/Platform.scala
@@ -4,12 +4,18 @@ import moduload.Moduload
 import scribe.output.format.{ANSIOutputFormat, ASCIIOutputFormat, OutputFormat}
 import scribe.writer.{SystemWriter, Writer}
 
-import java.io.{BufferedReader, InputStreamReader}
+import java.io.{BufferedReader, File, InputStreamReader}
+import java.nio.file.Files
 import scala.concurrent.ExecutionContext
 import scala.util.Try
 
 object Platform extends PlatformImplementation {
-  private val maximumColumns: Int = 5000
+  var maximumColumns: Int = 5000
+  var minimumColumns: Int = 10
+  var minimumRows: Int = 5
+  var columnsOverride: Option[Int] = None
+  var rowsOverride: Option[Int] = None
+
   private var lastChecked: Long = 0L
   private var cachedColumns: Int = -1
   private var cachedRows: Int = -1
@@ -39,9 +45,9 @@ object Platform extends PlatformImplementation {
 
   override def consoleWriter: Writer = SystemWriter
 
-  override def columns: Int = {
+  override def columns: Int = columnsOverride.getOrElse {
     updateConsoleSize()
-    if (cachedColumns == -1) {
+    if (cachedColumns < minimumColumns) {
       maximumColumns
     } else {
       cachedColumns + columnsAdjust
@@ -49,9 +55,13 @@ object Platform extends PlatformImplementation {
   }
 
 
-  override def rows: Int = {
+  override def rows: Int = rowsOverride.getOrElse {
     updateConsoleSize()
-    cachedRows
+    if (cachedRows < minimumRows) {
+      -1
+    } else {
+      cachedRows
+    }
   }
 
   private def updateConsoleSize(): Unit = {
@@ -72,6 +82,37 @@ object Platform extends PlatformImplementation {
       val columns = i.readLine().trim.toInt
       val rows = i.readLine().trim.toInt
       (columns, rows)
+    } finally {
+      i.close()
+    }
+  }.getOrElse((-1, -1))
+
+  private lazy val scriptFile = new File(System.getProperty("user.home"), ".cursor-position.sh")
+
+  def cursor(): (Int, Int) = Try {
+    if (!scriptFile.isFile) {
+      val script =
+        """#!/bin/bash
+          |exec < /dev/tty
+          |oldstty=$(stty -g)
+          |stty raw -echo min 0
+          |echo -en "\033[6n" > /dev/tty
+          |IFS=';' read -r -d R -a pos
+          |stty $oldstty
+          |row=$((${pos[0]:2} - 1))
+          |col=$((${pos[1]} - 1))
+          |echo $row $col""".stripMargin
+      Files.write(scriptFile.toPath, script.getBytes("UTF-8"))
+      val pb = new ProcessBuilder("bash", "-c", s"chmod +x ${scriptFile.getCanonicalPath}")
+      pb.start().waitFor()
+    }
+    val pb = new ProcessBuilder("bash", "-c", scriptFile.getCanonicalPath)
+    val p = pb.start()
+    val i = new BufferedReader(new InputStreamReader(p.getInputStream))
+    try {
+      val line = i.readLine()
+      val (row, col) = line.splitAt(line.indexOf(' '))
+      (row.trim.toInt, col.trim.toInt)
     } finally {
       i.close()
     }
