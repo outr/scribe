06752be61b3a803762bb3d20d8b2a63a9aff91ff
Matt Hicks <matt@matthicks.com>
2023-05-25 13:38:48 -0400

    Added preliminary timing logging features for cats-effect

--- cats/shared/src/main/scala/scribe/cats.scala
+++ cats/shared/src/main/scala/scribe/cats.scala
@@ -1,6 +1,7 @@
 package scribe
 
 import _root_.cats.effect._
+import perfolation._
 
 object cats {
   lazy val io: Scribe[IO] = apply[IO]
@@ -9,6 +10,34 @@ object cats {
   implicit class LoggerExtras(val logger: Logger) extends AnyVal {
     def f[F[_]](implicit sync: Sync[F]): Scribe[F] = new LoggerWrapper[F](logger, sync)
   }
+  implicit class IOExtras[Return](val io: IO[Return]) extends AnyVal {
+    def timed(label: String)(implicit timer: Timer): IO[Return] = timer.chain(io, label)
+  }
+
+  def timer[Return](f: Timer => IO[Return]): IO[Return] = for {
+    timer <- IO.blocking(Timer(System.currentTimeMillis()))
+    r <- f(timer)
+  } yield r
 
   def apply[F[_]: Sync]: Scribe[F] = new ScribeImpl[F](implicitly[Sync[F]])
+
+  case class Timer(start: Long) { self =>
+    private var last: Long = start
+
+    def log(label: String): IO[Unit] = for {
+      now <- IO.blocking(System.currentTimeMillis())
+      elapsed = ((now - start) / 1000.0).f(f = 3)
+      previous = ((now - last) / 1000.0).f(f = 3)
+      _ <- io.info(s"$label (Elapsed: $elapsed seconds, Since Previous: $previous seconds)")
+      _ <- IO.blocking {
+        self.synchronized {
+          last = now
+        }
+      }
+    } yield ()
+
+    def chain[Return](prev: IO[Return], label: String): IO[Return] = prev.flatMap { r =>
+      log(label).map(_ => r)
+    }
+  }
 }
